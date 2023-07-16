12034155a717bd3051b0fbfc1bfea1550d460b32
Mrcubix <tintamare91@gmail.com>
2023-07-16 15:53:25 +0200

    Made changes to build files

--- build.bat
+++ build.bat
@@ -2,13 +2,17 @@ dotnet restore
 
 dotnet publish -c Release -o temp
 
+del /Q build
+
 mkdir build
 
-move /Y temp\OTD.Backport.dll build\OTD.Backport.Parsers.dll
-move /Y temp\OTD.Backport.dll build\OTD.Backport.Parsers.pdb
-move /Y temp\OTD.Backport.dll build\OTD.Backport.Configurations.dll
-move /Y temp\OTD.Backport.pdb build\OTD.Backport.Configurations.pdb
+move /Y temp\OTD.Backport.Parsers.dll build\OTD.Backport.Parsers.dll
+move /Y temp\OTD.Backport.Parsers.pdb build\OTD.Backport.Parsers.pdb
+move /Y temp\OTD.Backport.Configurations.dll build\OTD.Backport.Configurations.dll
+move /Y temp\OTD.Backport.Configurations.pdb build\OTD.Backport.Configurations.pdb
 
 del /Q temp
 
 robocopy OTD.Backport.Configurations/Configurations build/Configurations /E /NFL /NDL /NJH /NJS /NC /NS
+
+7z a -r ./build/OTD.Backport.zip ./build/*
